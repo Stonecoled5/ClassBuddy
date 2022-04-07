206e30c0073f8797968db7c2fedb066f56b334bc
ddjacobson <jacobsondane@icloud.com>
2022-04-06 23:55:50 -0500

    Begun work on algorithm, solidified endpoints

--- ClassBuddy/src/main/java/com/app/ClassBuddy/database/daos/StudentDAO.java
+++ ClassBuddy/src/main/java/com/app/ClassBuddy/database/daos/StudentDAO.java
@@ -1,7 +1,9 @@
 package com.app.ClassBuddy.database.daos;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.app.ClassBuddy.database.documents.Course;
 import com.app.ClassBuddy.database.documents.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentDAO {
+    
     @Autowired
     private MongoTemplate mongoTemplate;
 
@@ -23,14 +26,58 @@ public class StudentDAO {
         mongoTemplate.insertAll(students);
     }
 
-    public Student findById(final String productId){
-        return mongoTemplate.findById(productId, Student.class);
+    public Student findById(final String studentId){
+        return mongoTemplate.findById(studentId, Student.class);
     }
 
     public Student findByEmail(final String email){
         return mongoTemplate.findOne(BasicQuery.query(Criteria.where("email").is(email)), Student.class);
     }
 
+    public void addCourseToSchedule(String email, Course c) {
+
+        Student s = findByEmail(email);
+        if (s != null) {
+
+            // check if course is in schedule
+            if (s.getSchedule().contains(c)) {
+                return;
+            }
+            s.setSchedule(c);
+            mongoTemplate.save(s);
+        }
+    }
+    
+    public void algorithm() {
+       List<Student> students = findAll();
+
+       for (Student s : students){
+            // iterating through entire document... find schedules
+            for (Course c : s.getSchedule()) {
+                if ()
+            }
+       }
+       
+    }
+
+    public void setFirstName(String email, String name) {
+        Student s = findByEmail(email);
+        if (s != null){
+            s.setFirstName(name);
+            mongoTemplate.save(s);
+            System.out.println(s.getFirstName());
+        }
+    }
+
+    public void setLastName(String email, String name) {
+        Student s = findByEmail(email);
+        if (s != null){
+            s.setLastName(name);
+            mongoTemplate.save(s);
+        }
+    }
+
+    
     public void save(Student student){
         mongoTemplate.save(student);
     }
